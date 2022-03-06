81ccfaae66e3d3e179fac4e0b8bdfa7b129c86fd
Evren Tan <evrentan@gmail.com>
2022-03-06 22:53:50 +0300

    feat: Add Javadoc
    feat: Add Javadoc

--- src/main/java/evrentan/examples/springbootprojectexample/message/MessageUtilityServiceImpl.java
+++ src/main/java/evrentan/examples/springbootprojectexample/message/MessageUtilityServiceImpl.java
@@ -12,31 +12,79 @@ public class MessageUtilityServiceImpl {
   private static final Logger logger = LogManager.getLogger(MessageUtilityServiceImpl.class);
   private static final Locale defaultLocale = new Locale("en");
 
+  /**
+   * Get the bundle for the given locale.
+   *
+   * @param locale locale of the bundle
+   * @return ResourceBundle related to the locale
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   * @since 1.0.0
+   */
   public static ResourceBundle getBundle(Locale locale) {
-    if(locale == null) {
+    if (locale == null) {
       locale = Optional.of(Locale.getDefault()).orElse(defaultLocale);
     }
     return ResourceBundle.getBundle("messages", locale);
   }
 
+  /**
+   * Get the message for the given key.
+   *
+   * @param messageKey key of the message
+   * @return String message
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   * @since 1.0.0
+   */
   public static String getMessage(String messageKey) {
     String message = "messageKey not found !!!";
     try {
       message = getBundle(null).getString(messageKey);
-    }catch (Exception e) {
+    } catch (Exception e) {
       logger.error(String.format("Error while getting message for messageKey %s", messageKey), e);
     }
     return message;
   }
 
+  /**
+   * Get the message for the given key and locale.
+   *
+   * @param messageKey key of the message
+   * @param locale locale of the message
+   * @return String message
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   * @since 1.0.0
+   */
   public static String getMessage(String messageKey, Locale locale) {
     return getBundle(locale).getString(messageKey);
   }
 
+  /**
+   * Get the message for the given key and arguments.
+   * @param messageKey key of the message
+   * @param messageArguments arguments of the message
+   * @return String message
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   * @since 1.0.0
+   */
   public static String getMessage(String messageKey, Object... messageArguments) {
     return getMessage(messageKey, messageArguments, null);
   }
 
+  /**
+   * Get the message for the given key, locale and arguments.
+   *
+   * @param messageKey key of the message
+   * @param locale locale of the message
+   * @param messageArguments arguments of the message
+   * @return String message
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   * @since 1.0.0
+   */
   public static String getMessage(String messageKey, Locale locale, Object... messageArguments) {
     return MessageFormat.format(getBundle(locale).getString(messageKey), messageArguments);
   }
